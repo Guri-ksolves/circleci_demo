e6b3929ce8d9e0349421b0a0fb39a8977c716c56
Atul Khanduri <atul91khanduri@gmail.com>
2017-07-11 00:34:30 +0530

    Added test

---
+++ spec/hello_world_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.feature "Contact", :type => :feature do
+  scenario "Create a new contact" do
+    visit "/"
+
+    expect(page).to have_text("Hello World")
+  end
+end
