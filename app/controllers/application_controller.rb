eb0d4071f3ef15007be04d7e19a8da536f72029d
Atul Khanduri <atul91khanduri@gmail.com>
2017-07-15 00:44:09 +0530

    Minor change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render plain: "Hello World."
+    render plain: "Hello World...."
   end
 end
